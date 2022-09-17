05d961df59a4d92da8dbccbc658e4f81144f17b9
rike <hello@rike.dev>
2022-09-17 17:59:28 +0200

    fix?

--- frontend/src/pages/Chapter.tsx
+++ frontend/src/pages/Chapter.tsx
@@ -49,7 +49,7 @@ const NFTMint = () => {
         }}
         className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
       >
-        Weiter
+        Jetzt minten
       </button>
     </>
   );
