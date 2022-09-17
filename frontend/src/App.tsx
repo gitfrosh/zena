fcd1b29ed618bc09b4e17a711b6a75a7b9750dbf
Astrid Varga <astrid.varga@aleri.de>
2022-09-17 21:28:03 +0200

    add right image to first screen

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -12,13 +12,13 @@ export default function App() {
 
   const Start = () => {
     return (
-      <div className="relative overflow-hidden bg-white">
+      <div className="relative overflow-hidden bg-white h-screen">
         <div className="mx-auto max-w-7xl">
-          <div className="relative z-10 bg-white pb-8 sm:pb-16 md:pb-20 lg:w-full lg:max-w-2xl lg:pb-28 xl:pb-32">
+          <div className="z-10 bg-white pb-8 sm:pb-16 md:pb-20 lg:w-full lg:max-w-2xl lg:pb-28 xl:pb-32">
             <main className="mx-auto mt-10 max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
               <div className="sm:text-center lg:text-left">
                 <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl md:text-6xl">
-                  <span className="block text-indigo-600 xl:inline">Z3na</span>{' '}
+                  <span className="block text-indigo-600 xl:inline">ZENA</span>{' '}
                   <span className="block xl:inline">Dein Start ins Web3 </span>
                 </h1>
                 <p className="mt-3 text-base text-gray-500 sm:mx-auto sm:mt-5 sm:max-w-xl sm:text-lg md:mt-5 md:text-xl lg:mx-0">
@@ -50,7 +50,7 @@ export default function App() {
         <div className="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
           <img
             className="h-56 w-full object-cover sm:h-72 md:h-96 lg:h-full lg:w-full"
-            src="https://images.unsplash.com/photo-1551434678-e076c223a692?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2850&q=80"
+            src="/Onboarding-1.jpeg"
             alt=""
           />
         </div>
